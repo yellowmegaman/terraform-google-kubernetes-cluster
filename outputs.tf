45a6f87f86c11b4d28b7b968c86000ab28d2b7e0
emik0 <yellowmegaman@gmail.com>
2019-03-27 01:12:53 +0300

    remove outputs for now

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-output "endpoint" {
-  value = ["${google_container_cluster.gke-cluster.primary.endpoint}"]
-}
-output "name" {
-  value = ["${google_container_cluster.gke-cluster.primary.name}"]
-}
