f7afcdbbb9d3c68633bf4725ce164d8a736612d3
yellowmegaman <yellowmegaman@workbox>
2019-07-22 13:33:30 +0300

    terraform 0.12 compat

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
 resource "google_container_cluster" "gke-cluster" {
-  name                     = "${var.name}"
-  location                 = "${var.zone}"
+  name                     = var.name
+  location                 = var.zone
   remove_default_node_pool = true
   initial_node_count       = 1
-  min_master_version       = "${var.kube_version}"
+  min_master_version       = var.kube_version
   enable_legacy_abac       = false
+  maintenance_policy {
+    daily_maintenance_window {
+      start_time = var.maint_window
+    }
+  }
   master_auth {
     username = ""
     password = ""
     client_certificate_config {
-      issue_client_certificate = false
+      issue_client_certificate = var.client_cert
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -10,3 +10,13 @@ variable "kube_version" {
 	default     = "1.12.5-gke.5"
 	description = "Desired GKE kubernetes version. Refer to https://cloud.google.com/kubernetes-engine/docs/release-notes"
 }
+
+variable "maint_window" {
+	default     = "01:00"
+	description = "Desired time to conduct maint operations"
+}
+
+variable "client_cert" {
+	default     = "true"
+	description = "Issue client_certificate true/false"
+}
